3852555824592a23d17e2dc7b8a9b7c7ec554084
Andrew Hyde <andrewryanhyde@hotmail.co.uk>
2018-06-26 09:01:23 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,18 @@
+class Artist 
+  attr_accessor :name, :genre
+
+  def initialize(name, genre)
+    @name = name 
+    @genre = genre
+  end
+
+
+  
+end
+
+
+beyonce = Artist.new("Beyonce", "R&B")
+
+halo = Song.new("Beyonce", "Halo")
+
+halo.Artist = beyonce
